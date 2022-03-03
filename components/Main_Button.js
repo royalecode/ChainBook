5d51560d1b526efa5a9bb452d23f2dccd91c7ac1
Edmon Bosch <edmonbosch@gmail.com>
2022-03-03 19:22:24 +0100

    home_page_section1

---
+++ components/Main_Button.js
@@ -0,0 +1,5 @@
+export default function Main_Button ({title}) {
+    if (!title) return null
+    
+    return <button>{title}</button>
+    }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+//import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    /*
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+    */
+    <Component {...pageProps} />
+
+  );
 }
 
-export default MyApp
+export default MyApp;
